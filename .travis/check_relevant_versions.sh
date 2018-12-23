01244c375c62016cae89099c6549dc6422229267
Oskar Stark <oskarstark@googlemail.com>
2018-12-23 14:11:36 +0100

    temp travis remove from check

--- .travis/check_relevant_versions.sh
+++ .travis/check_relevant_versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 
-RELEVANT_FILES=$(git diff --name-only HEAD upstream/${TRAVIS_BRANCH} -- '${PHP_VERSION}/Dockerfile' '.travis.yml')
+RELEVANT_FILES=$(git diff --name-only HEAD upstream/${TRAVIS_BRANCH} -- '${PHP_VERSION}/Dockerfile')
 
 if [[ -z ${RELEVANT_FILES} ]]; then echo -n 'KO'; exit 0; fi;
