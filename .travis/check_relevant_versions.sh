546d583bc1f122bd232307a6d20e1f98ecb67f25
Oskar Stark <oskarstark@googlemail.com>
2018-12-23 13:50:35 +0100

    remove echo

--- .travis/check_relevant_versions.sh
+++ .travis/check_relevant_versions.sh
@@ -3,7 +3,4 @@ set -ev
 
 RELEVANT_FILES=$(git diff --name-only HEAD upstream/${TRAVIS_BRANCH} -- '${PHP_VERSION}/Dockerfile' '.travis.yml')
 
-
-echo $RELEVANT_FILES
-
 if [[ -z ${RELEVANT_FILES} ]]; then echo -n 'KO'; exit 0; fi;
