09cf00949796512767ee48ad03083d0c60c2293c
Jan Schütze <jan.schuetze@exozet.com>
2019-12-11 16:05:06 +0100

    Added alpine image for 7.4 #92

---
+++ build_alpine_version.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -e
+
+PHP_VERSION=$1
+
+if [ -z "$PHP_VERSION" ]
+then
+  echo "Error: Missing version name!" >&2
+  echo "Correct Usage $0 PHP_VERSION (e.g. $0 7.0.16)" >&2
+  exit 1;
+fi
+
+FOLDER=`echo $PHP_VERSION | cut -f 1,2 -d '.'`
+
+if [ ! -d $FOLDER ]
+then
+  echo "Error: Cannot root folder $FOLDER for php version $PHP_VERSION" >&2
+  exit 1;
+fi
+
+cd $FOLDER
+
+cp ../php.ini php.ini
+cp ../start-cron start-cron
+cp ../start-cron-alpine start-cron-alpine
+
+echo "Pull latest php:${PHP_VERSION}-fpm-alpine"
+docker pull "php:${PHP_VERSION}-fpm-alpine" >> ../build.log
+
+echo "Building exozet/php-fpm:${PHP_VERSION}-alpine"
+
+echo "FROM php:${PHP_VERSION}-fpm-alpine AS build-base" >> version-Dockerfile
+cat Dockerfile-alpine | grep -v '^FROM' >> version-Dockerfile
+
+docker build -t exozet/php-fpm:${PHP_VERSION}-alpine -f version-Dockerfile . >> ../build.log
+
+rm -f version-Dockerfile
+
+echo "FROM php:${PHP_VERSION}-fpm-alpine AS build-base" >> sudo-Dockerfile
+cat Dockerfile-alpine | grep -v '^FROM' >> sudo-Dockerfile
+echo '' >> sudo-Dockerfile
+echo 'RUN apk add --no-cache sudo' >> sudo-Dockerfile
+echo "RUN sed -i 's/\/home\/www-data:\/sbin\/nologin/\/home\/www-data:\/bin\/bash/' /etc/passwd" >> sudo-Dockerfile
+echo 'RUN echo "www-data ALL = NOPASSWD: ALL" > /etc/sudoers.d/www-data' >> sudo-Dockerfile
+
+echo "Building exozet/php-fpm:${PHP_VERSION}-sudo-alpine"
+docker build -t exozet/php-fpm:${PHP_VERSION}-sudo-alpine -f sudo-Dockerfile . >> ../build.log
+
+
+echo "FROM php:${PHP_VERSION}-fpm-alpine AS build-base" >> sudo-Dockerfile
+cp sudo-Dockerfile xdebug-Dockerfile
+echo '' >> xdebug-Dockerfile
+
+
+RUN
+
+echo 'COPY --from=build-xdebug /tmp/xdebug.so /tmp/xdebug.so' >> xdebug-Dockerfile
+echo "RUN mv /tmp/xdebug.so \`php -i | grep ^extension_dir | cut -f 3 -d ' '\`/ && docker-php-ext-enable xdebug" >> xdebug-Dockerfile
+
+echo "Building exozet/php-fpm:${PHP_VERSION}-xdebug-alpine"
+docker build -t exozet/php-fpm:${PHP_VERSION}-xdebug-alpine -f xdebug-Dockerfile . >> ../build.log
+
+rm -f sudo-Dockerfile
+rm -f xdebug-Dockerfile
+
+echo "FROM php:${PHP_VERSION}-fpm-alpine AS build-base" >> root-Dockerfile
+cat Dockerfile-alpine | grep -v '^FROM' >> root-Dockerfile
+echo '' >> root-Dockerfile
+echo 'USER root' >> root-Dockerfile
+
+echo "Building exozet/php-fpm:${PHP_VERSION}-root-alpine"
+docker build -t exozet/php-fpm:${PHP_VERSION}-root-alpine -f root-Dockerfile . >> ../build.log
+
+rm -f root-Dockerfile
+rm -f php.ini
--- push_version.sh
+++ push_version.sh
@@ -14,10 +14,18 @@ fi
 
 echo "Pushing exozet/php-fpm:${PHP_VERSION}"
 docker push exozet/php-fpm:${PHP_VERSION}
+echo "Pushing exozet/php-fpm:${PHP_VERSION}-alpine"
+docker push exozet/php-fpm:${PHP_VERSION}-alpine
 echo "Pushing exozet/php-fpm:${PHP_VERSION}-sudo"
 docker push exozet/php-fpm:${PHP_VERSION}-sudo
+echo "Pushing exozet/php-fpm:${PHP_VERSION}-sudo-alpine"
+docker push exozet/php-fpm:${PHP_VERSION}-sudo-alpine
 echo "Pushing exozet/php-fpm:${PHP_VERSION}-root"
 docker push exozet/php-fpm:${PHP_VERSION}-root
+echo "Pushing exozet/php-fpm:${PHP_VERSION}-root-alpine"
+docker push exozet/php-fpm:${PHP_VERSION}-root-alpine
+echo "Pushing exozet/php-fpm:${PHP_VERSION}-xdebug-alpine"
+docker push exozet/php-fpm:${PHP_VERSION}-xdebug-alpine
 
 if [ "$PHP_VERSION_ALIAS" ]
 then
@@ -28,13 +36,33 @@ then
   echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}"
   docker push exozet/php-fpm:${PHP_VERSION_ALIAS}
 
+  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-alpine based on exozet/php-fpm:${PHP_VERSION}-alpine"
+  docker tag exozet/php-fpm:${PHP_VERSION}-alpine exozet/php-fpm:${PHP_VERSION_ALIAS}-alpine
+  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-alpine"
+  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-alpine
+
   echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo based on exozet/php-fpm:${PHP_VERSION}-sudo"
   docker tag exozet/php-fpm:${PHP_VERSION}-sudo exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo
   echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo"
   docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo
 
+  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo-alpine based on exozet/php-fpm:${PHP_VERSION}-sudo-alpine"
+  docker tag exozet/php-fpm:${PHP_VERSION}-sudo-alpine exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo-alpine
+  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo-alpine"
+  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo-alpine
+
   echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-root based on exozet/php-fpm:${PHP_VERSION}-root"
   docker tag exozet/php-fpm:${PHP_VERSION}-root exozet/php-fpm:${PHP_VERSION_ALIAS}-root
   echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-root"
   docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-root
+
+  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-root-alpine based on exozet/php-fpm:${PHP_VERSION}-root-alpine"
+  docker tag exozet/php-fpm:${PHP_VERSION}-root-alpine exozet/php-fpm:${PHP_VERSION_ALIAS}-root-alpine
+  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-root-alpine"
+  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-root-alpine
+
+  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-xdebug-alpine based on exozet/php-fpm:${PHP_VERSION}-xdebug-alpine"
+  docker tag exozet/php-fpm:${PHP_VERSION}-xdebug-alpine exozet/php-fpm:${PHP_VERSION_ALIAS}-xdebug-alpine
+  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-xdebug-alpine"
+  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-xdebug-alpine
 fi
