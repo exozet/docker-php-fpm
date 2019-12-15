8d0df21415cbaa66d69eb9ac5e92912a8de22fdc
Jan Schütze <jan.schuetze@exozet.com>
2019-12-15 08:38:50 +0100

    Tag and push quay images

--- push_version.sh
+++ push_version.sh
@@ -12,20 +12,20 @@ then
   exit 1;
 fi
 
+TAG_SUFFIXES="alpine sudo sudo-alpine root root-alpine xdebug-alpine"
+
 echo "Pushing exozet/php-fpm:${PHP_VERSION}"
 docker push exozet/php-fpm:${PHP_VERSION}
-echo "Pushing exozet/php-fpm:${PHP_VERSION}-alpine"
-docker push exozet/php-fpm:${PHP_VERSION}-alpine
-echo "Pushing exozet/php-fpm:${PHP_VERSION}-sudo"
-docker push exozet/php-fpm:${PHP_VERSION}-sudo
-echo "Pushing exozet/php-fpm:${PHP_VERSION}-sudo-alpine"
-docker push exozet/php-fpm:${PHP_VERSION}-sudo-alpine
-echo "Pushing exozet/php-fpm:${PHP_VERSION}-root"
-docker push exozet/php-fpm:${PHP_VERSION}-root
-echo "Pushing exozet/php-fpm:${PHP_VERSION}-root-alpine"
-docker push exozet/php-fpm:${PHP_VERSION}-root-alpine
-echo "Pushing exozet/php-fpm:${PHP_VERSION}-xdebug-alpine"
-docker push exozet/php-fpm:${PHP_VERSION}-xdebug-alpine
+docker tag exozet/php-fpm:${PHP_VERSION} quay.io/exozet/php-fpm:${PHP_VERSION}
+docker push quay.io/exozet/php-fpm:${PHP_VERSION}
+
+for TAG_SUFFIX in $TAG_SUFFIXES
+do
+  echo "Pushing exozet/php-fpm:${PHP_VERSION}-${TAG_SUFFIX}"
+  docker push exozet/php-fpm:${PHP_VERSION}-${TAG_SUFFIX}
+  docker tag exozet/php-fpm:${PHP_VERSION}-${TAG_SUFFIX} quay.io/exozet/php-fpm:${PHP_VERSION}-${TAG_SUFFIX}
+  docker push quay.io/exozet/php-fpm:${PHP_VERSION}-${TAG_SUFFIX}
+done
 
 if [ "$PHP_VERSION_ALIAS" ]
 then
@@ -36,33 +36,11 @@ then
   echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}"
   docker push exozet/php-fpm:${PHP_VERSION_ALIAS}
 
-  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-alpine based on exozet/php-fpm:${PHP_VERSION}-alpine"
-  docker tag exozet/php-fpm:${PHP_VERSION}-alpine exozet/php-fpm:${PHP_VERSION_ALIAS}-alpine
-  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-alpine"
-  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-alpine
-
-  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo based on exozet/php-fpm:${PHP_VERSION}-sudo"
-  docker tag exozet/php-fpm:${PHP_VERSION}-sudo exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo
-  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo"
-  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo
-
-  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo-alpine based on exozet/php-fpm:${PHP_VERSION}-sudo-alpine"
-  docker tag exozet/php-fpm:${PHP_VERSION}-sudo-alpine exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo-alpine
-  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo-alpine"
-  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-sudo-alpine
-
-  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-root based on exozet/php-fpm:${PHP_VERSION}-root"
-  docker tag exozet/php-fpm:${PHP_VERSION}-root exozet/php-fpm:${PHP_VERSION_ALIAS}-root
-  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-root"
-  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-root
-
-  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-root-alpine based on exozet/php-fpm:${PHP_VERSION}-root-alpine"
-  docker tag exozet/php-fpm:${PHP_VERSION}-root-alpine exozet/php-fpm:${PHP_VERSION_ALIAS}-root-alpine
-  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-root-alpine"
-  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-root-alpine
-
-  echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-xdebug-alpine based on exozet/php-fpm:${PHP_VERSION}-xdebug-alpine"
-  docker tag exozet/php-fpm:${PHP_VERSION}-xdebug-alpine exozet/php-fpm:${PHP_VERSION_ALIAS}-xdebug-alpine
-  echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-xdebug-alpine"
-  docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-xdebug-alpine
+  for TAG_SUFFIX in $TAG_SUFFIXES
+  do
+    echo "Tagging exozet/php-fpm:${PHP_VERSION_ALIAS}-${TAG_SUFFIX} based on exozet/php-fpm:${PHP_VERSION}-${TAG_SUFFIX}"
+    docker tag exozet/php-fpm:${PHP_VERSION}-${TAG_SUFFIX} exozet/php-fpm:${PHP_VERSION_ALIAS}-${TAG_SUFFIX}
+    echo "Pushing exozet/php-fpm:${PHP_VERSION_ALIAS}-${TAG_SUFFIX}"
+    docker push exozet/php-fpm:${PHP_VERSION_ALIAS}-${TAG_SUFFIX}
+  done
 fi
